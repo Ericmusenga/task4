4325fbdd6633e714b257b326f79b8ad9c60623a3
Eric <museric97@gmail.com>
2025-01-29 20:54:42 +0200

    index page is created.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// src/index.php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hospital Management System</title>
+</head>
+<body>
+    <h2>Welcome to the Hospital Management System</h2>
+    <nav>
+        <ul>
+            <li><a href="register.php">Register</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
